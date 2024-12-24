b72198c19cf95e1cbaf06981601b2a53dadf4dda
pamelaalvarezf <carlotalova17@gmail.com>
2024-12-24 10:47:48 -0400

    funcion_menu

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
   const menuToggle = document.getElementById('menuToggle');
   const closeMenu = document.getElementById('closeMenu');
   const slideMenu = document.getElementById('slideMenu');
   const menuOverlay = document.getElementById('menuOverlay');
 
-  // Open Menu
+  // Abrir el menú
   menuToggle.addEventListener('click', () => {
       slideMenu.classList.add('active');
       menuOverlay.classList.add('active');
   });
 
-  // Close Menu
+  // Cerrar el menú
   closeMenu.addEventListener('click', () => {
       slideMenu.classList.remove('active');
       menuOverlay.classList.remove('active');
   });
 
-  // Smooth Scroll for Links
+  menuOverlay.addEventListener('click', () => {
+      slideMenu.classList.remove('active');
+      menuOverlay.classList.remove('active');
+  });
+
+  // Smooth Scroll
   const links = document.querySelectorAll('a[href^="#"]');
   links.forEach(link => {
-      link.addEventListener('click', function (event) {
-          event.preventDefault();
-          const targetID = this.getAttribute('href').substring(1);
+      link.addEventListener('click', (e) => {
+          e.preventDefault();
+          const targetID = link.getAttribute('href').substring(1);
           const targetSection = document.getElementById(targetID);
           if (targetSection) {
-              scroll.scrollTo(targetSection);
+              targetSection.scrollIntoView({ behavior: 'smooth' });
               slideMenu.classList.remove('active');
               menuOverlay.classList.remove('active');
           }
