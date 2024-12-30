16adee0830fbcbb2572f9067238210bf2aab503a
Hannie <hbefeler@gmail.com>
2024-12-30 13:57:05 +0100

    About WIP

--- js/script.js
+++ js/script.js
@@ -23,3 +23,48 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// WRAP TEXT
+document.addEventListener("DOMContentLoaded", function() {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml10 .letters');
+    if (textWrapper) {
+        textWrapper.innerHTML = textWrapper.textContent.replace(/./g, "<span class='letter'>$&</span>");
+    
+        anime.timeline({loop: true})
+          .add({
+            targets: '.ml10 .letter',
+            rotateY: [-90, 0],
+            duration: 1300,
+            delay: (el, i) => 45 * i
+          }).add({
+            targets: '.ml10',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+          });
+    }
+});
+
+// IMAGE SLIDE
+
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.image-slider img');
+const totalImages = images.length;
+
+function changeImage() {
+    // Reset all images to opacity 0
+    images.forEach(img => img.style.opacity = '0');
+    // Set current image to opacity 1
+    images[currentImageIndex].style.opacity = '1';
+    
+    // Increment image index, reset to 0 if last image is reached
+    currentImageIndex = (currentImageIndex + 1) % totalImages;
+}
+
+// Change image every 3 seconds
+setInterval(changeImage, 3000);
+
+// Initialize first image as visible
+changeImage();
+
