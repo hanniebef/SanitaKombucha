269236d2256f81f075d062a634a17b3e80bd8419
pamelaalvarezf <pamelaalvarezfuentes@gmail.com>
2025-01-10 17:06:24 +0100

    fix/optimizer

--- js/script.js
+++ js/script.js
@@ -16,30 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-// WRAP TEXT
 document.addEventListener("DOMContentLoaded", function() {
-  // Select all elements with the 'letters' class
-  var textWrappers = document.querySelectorAll('.ml10 .letters');
-  
-  textWrappers.forEach(function(textWrapper) {
-      // Wrap every letter in a span for each text element
-      textWrapper.innerHTML = textWrapper.textContent.replace(/./g, "<span class='letter'>$&</span>");
-  });
+    // Wrap every letter in spans
+    document.querySelectorAll('.ml10 .letters').forEach(function(element) {
+        element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    });
 
-  // Create the animation
-  anime.timeline({loop: true})
-      .add({
-          targets: '.ml10 .letter',
-          rotateY: [-90, 0],
-          duration: 1300,
-          delay: (el, i) => 45 * i
-      }).add({
-          targets: '.ml10',
-          opacity: 0,
-          duration: 1000,
-          easing: "easeOutExpo",
-          delay: 1000
-      });
+    anime.timeline({loop: true})
+        .add({
+            targets: '.ml10 .letter',
+            rotateY: [-90, 0],
+            duration: 1300,
+            delay: (el, i) => 45 * i
+        })
+        .add({
+            targets: '.ml10',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
 });
 
 // IMAGE SLIDE
